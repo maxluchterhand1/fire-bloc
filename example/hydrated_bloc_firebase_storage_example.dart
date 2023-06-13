c06e80d81d003c16608fc02da33379a9a14037fe
Max Luchterhand <max.luchterhand@elmatrix.de>
2023-06-13 15:38:52 +0200

    feat: initial commit

---
+++ example/hydrated_bloc_firebase_storage_example.dart
@@ -0,0 +1,6 @@
+import 'package:hydrated_bloc_firebase_storage/hydrated_bloc_firebase_storage.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ test/hydrated_bloc_firebase_storage_test.dart
@@ -0,0 +1,16 @@
+import 'package:hydrated_bloc_firebase_storage/hydrated_bloc_firebase_storage.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
